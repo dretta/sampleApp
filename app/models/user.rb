ac11bdd43f68f73dbfe20ed9d4d3415397331409
Daniel Retta <dretta@ymail.com>
2015-02-28 23:47:12 -0800

    Accessed virtual remember_token in integration test Chapter 8.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,18 +11,16 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, length:{ minimum: 6}
 
-	class << self
-		# Returns the hash digest of the given string.
-		def digest(string)
-			cost = ActiveModel::SecurePassword.min_cost ? 
-				BCrypt::Engine::MIN_COST : 
-				BCrypt::Engine.cost
-			BCrypt::Password.create(string, cost: cost)
-		end
+	# Returns the hash digest of the given string.
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? 
+			BCrypt::Engine::MIN_COST : 
+			BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
 
-		def new_token
-			SecureRandom.urlsafe_base64
-		end
+	def User.new_token
+		SecureRandom.urlsafe_base64
 	end
 
 	# Remembers a user in the database for use in persistent sessions.
